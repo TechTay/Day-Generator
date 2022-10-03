31a8f63efa8a79ae3798c03c73a2eda27d81287d
DeVante Parker <Devante.parker15@gmail.com>
2022-10-02 23:45:05 -0500

    Update for Assignment

--- Main.py
+++ Main.py
@@ -10,15 +10,55 @@ random.choice(destination)
 
 
 
-restaurant = ['Popeyes', 'steakhouse', 'cajun', 'McDonalds']
+restaurant = ['Popeyes', 'Steakhouse', 'Cajun', 'McDonalds']
 random.choice(restaurant)
 
 
-mode_of_transportation = ['airplane', 'car', 'boat', 'Train']
+mode_of_transportation = ['Airplane', 'Car', 'Boat', 'Train']
 random.choice(mode_of_transportation)
 
 entertainment = ['Festival', 'Carnival', 'Museum', 'Six Flags']
 random.choice(entertainment)
 
 
-# def run_day_trip_generator():
+def day_trip_generator():
+    location = random.choice(destination)
+    print(f'Destination: {location} ')
+
+day_trip_generator()
+
+def food_places_generator():
+    food = random.choice(restaurant)
+    print(f'Restaurant: {food} ')
+
+food_places_generator()
+
+def transportation_generator():
+        transportation = random.choice(mode_of_transportation)
+        print(f'Transportation: {transportation} ')
+
+transportation_generator()
+
+def entertainment_generator():
+    your_entertainment = random.choice(entertainment)
+    print(f'Entertainment: {your_entertainment} ')
+
+entertainment_generator()
+
+def All_items_generator():
+    All_items = (entertainment_generator + transportation_generator + food_places_generator + day_trip_generator)
+    print(f'{All_items} ')
+
+x = input('Are you satisfied with your trip? Y or N ' '\n')
+
+if x == "N '\n' ":
+    print(f'need assistance...')
+else:
+    confirm_trip = False
+while confirm_trip:
+    break
+        
+print(f'Here is your final trip!{All_items_generator}')
+
+
+
