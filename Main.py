78759fd5e3c374a3e952d893f724f7af4ba42b61
DeVante Parker <Devante.parker15@gmail.com>
2022-10-02 20:09:19 -0500

    Day Generator Assignment

---
+++ Main.py
@@ -0,0 +1,13 @@
+Day_Trip_List = ['Destinations, restaurants, mode of transportation, entertainment']
+
+print(Day_Trip_List)
+
+destination = ['Hawaii, Florida, California']
+
+restaurant = ['West, steakhouse, cajun']
+
+mode_of_transportation = ['airplane, car, boat']
+
+entertainment = ['Festival, Carnival, Museum']
+
+print(entertainment)
