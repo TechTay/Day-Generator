9b61e20c12db82135bb8bd6db5fe44b03361a0fe
DeVante Parker <Devante.parker15@gmail.com>
2022-10-03 08:12:56 -0500

    Made Progress Towards Assignment

--- Main.py
+++ Main.py
@@ -1,13 +1,10 @@
 import random
 
 
-
 Day_Trip_Lists = ['Destinations', 'restaurants', 'mode of transportation', 'entertainment']
 
 destination = ['Hawaii ' ,'Florida ', 'California ', 'Connecticut ']
 random.choice(destination)
-
-
 
 
 restaurant = ['Popeyes', 'Steakhouse', 'Cajun', 'McDonalds']
@@ -22,8 +19,15 @@ random.choice(entertainment)
 
 
 def day_trip_generator():
-    location = random.choice(destination)
-    print(f'Destination: {location} ')
+    confirm_bool = True
+    while confirm_bool:
+        location = random.choice(destination)
+        location_input = input(f'Destination {location} was selected. Do you accept? Y or N ' '\n')
+        if location_input == 'N':
+            destination.remove(location)
+        else:
+            print(f'Destination: {location} ')
+            confirm_bool = False
 
 day_trip_generator()
 
@@ -46,19 +50,16 @@ def entertainment_generator():
 entertainment_generator()
 
 def All_items_generator():
-    All_items = (entertainment_generator + transportation_generator + food_places_generator + day_trip_generator)
+    All_items = random.choice(entertainment, destination, mode_of_transportation, restaurant)
     print(f'{All_items} ')
 
 x = input('Are you satisfied with your trip? Y or N ' '\n')
 
 if x == "N '\n' ":
-    print(f'need assistance...')
+    print(All_items_generator)
 else:
-    confirm_trip = False
-while confirm_trip:
-    break
         
-print(f'Here is your final trip!{All_items_generator}')
+    print(f'Here is your final trip!{All_items_generator}')
 
 
 
