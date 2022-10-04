e0be148844c58a51b79196c115b5a175a4edf6b7
DeVante Parker <Devante.parker15@gmail.com>
2022-10-04 15:08:14 -0500

    Day Generator Edit

--- Main.py
+++ Main.py
@@ -1,65 +1,83 @@
 import random
 
 
+
+
 Day_Trip_Lists = ['Destinations', 'restaurants', 'mode of transportation', 'entertainment']
 
-destination = ['Hawaii ' ,'Florida ', 'California ', 'Connecticut ']
+destination = ['Hawaii ' ,'Florida ', 'California ', 'Connecticut ', 'Oklahoma', 'New York']
 random.choice(destination)
 
 
-restaurant = ['Popeyes', 'Steakhouse', 'Cajun', 'McDonalds']
+restaurant = ['Popeyes', 'Steakhouse', 'Cajun', 'McDonalds', 'KFC', 'Chipotle']
 random.choice(restaurant)
 
 
-mode_of_transportation = ['Airplane', 'Car', 'Boat', 'Train']
+mode_of_transportation = ['Airplane', 'Car', 'Boat', 'Train', 'Biking','Scooter' ]
 random.choice(mode_of_transportation)
 
-entertainment = ['Festival', 'Carnival', 'Museum', 'Six Flags']
+entertainment = ['Festival', 'Carnival', 'Museum', 'Six Flags', 'Theatre', 'Sporting Event']
 random.choice(entertainment)
 
 
 def day_trip_generator():
-    confirm_bool = True
-    while confirm_bool:
-        location = random.choice(destination)
-        location_input = input(f'Destination {location} was selected. Do you accept? Y or N ' '\n')
-        if location_input == 'N':
-            destination.remove(location)
-        else:
-            print(f'Destination: {location} ')
-            confirm_bool = False
+    location = random.choice(destination)
+    return location
+
+result_of_location = day_trip_generator()
+print(f'Destination: {result_of_location} ')
 
 day_trip_generator()
 
 def food_places_generator():
     food = random.choice(restaurant)
-    print(f'Restaurant: {food} ')
+    return food
+
+result_of_food = food_places_generator()
+print(f'Restaurant: {result_of_food} ')
 
 food_places_generator()
 
 def transportation_generator():
-        transportation = random.choice(mode_of_transportation)
-        print(f'Transportation: {transportation} ')
+    transportation = random.choice(mode_of_transportation)
+    return transportation
+
+result_of_transportation = transportation_generator()
+print(f'Transportation: {result_of_transportation} ')
 
 transportation_generator()
 
 def entertainment_generator():
     your_entertainment = random.choice(entertainment)
-    print(f'Entertainment: {your_entertainment} ')
+    return your_entertainment
+
+result_of_entertainment = entertainment_generator()
+print(f'Entertainment: {result_of_entertainment} ')
 
 entertainment_generator()
 
 def All_items_generator():
-    All_items = random.choice(entertainment, destination, mode_of_transportation, restaurant)
-    print(f'{All_items} ')
+    All_items = random.choice(day_trip_generator, entertainment_generator,transportation_generator, food_places_generator)
+    return All_items
 
-x = input('Are you satisfied with your trip? Y or N ' '\n')
 
-if x == "N '\n' ":
-    print(All_items_generator)
-else:
+    
+    
+
+       
+# if location == 'N'
+# else:
+#             print(f'Destination: {location} ')
         
-    print(f'Here is your final trip!{All_items_generator}')
+
+# x = input('Are you satisfied with your trip? Y or N ' '\n')
+
+# if x == "N '\n' ":
+#     print()
+#     pass
+# else:
+        
+#     print(f'Here is your final trip!{All_items_generator}')
 
 
 
